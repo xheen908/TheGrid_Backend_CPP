91765b4bd948f24a9371794c17cb862703e8e3c2
xheen908 <xheen908@gmail.com>
2026-01-31 00:31:47 +0100

    Fix: Remove build and public folders from version control

--- build/CMakeFiles/fc-tmp/mariadb-connector-c/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for mariadb-connector-c")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/xheen908/Documents/thegrid_backend_c/build/CMakeFiles/fc-tmp/mariadb-connector-c/mariadb-connector-c-gitclone.cmake")
-
-endblock()
